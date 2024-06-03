9d8cdcf22aaf4fbaf28d9a5b967ed7e0a0b9fe7a
Batuhan TANIR <batuhan.tanir00@gmail.com>
2024-06-03 11:09:36 +0300

    Fix input handling and memory leaks

--- checker_bonus.c
+++ checker_bonus.c
@@ -6,7 +6,7 @@
 /*   By: btanir <btanir@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 18:01:58 by btanir            #+#    #+#             */
-/*   Updated: 2024/06/01 14:01:19 by btanir           ###   ########.fr       */
+/*   Updated: 2024/06/03 11:09:10 by btanir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ int	main(int ac, char **av)
 {
 	t_stacks	*stacks;
 	char		*line;
+	char		*temp;
+	char		*temp2;
+	char		**action_list;
 
 	stacks = new_stacks();
 	if (ac < 2)
@@ -29,16 +32,31 @@ int	main(int ac, char **av)
 	check_args(stacks);
 	add_args_to_stack(stacks);
 	line = get_next_line(0);
-	while (line != NULL)
+	while (1)
 	{
-		line[ft_strlen(line) - 1] = '\0';
-		actions(stacks, line);
+		temp2 = get_next_line(0);
+		temp = ft_strjoin(line, temp2);
+		if (temp2 == NULL)
+		{
+			free(temp2);
+			free(line);
+			break ;
+		}
+		free(temp2);
 		free(line);
-		line = get_next_line(0);
+		line = temp;
+	}
+	action_list = ft_split(line, '\n');
+	ft_printf("%s\n", action_list[1]);
+	while (*action_list)
+	{
+		actions(stacks, *action_list);
+		action_list++;
 	}
 	if (is_sort(stacks->stack_a) && stacks->count_b == 0)
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
+	// system("leaks checker");
 	return (0);
 }
